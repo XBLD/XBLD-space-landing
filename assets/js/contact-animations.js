a7813a3b9073cf1c1e90e10d4457a7d54eab0f13
a-axton <dizzysfarm@gmail.com>
2017-01-23 14:45:04 -0800

    added contact out animations

--- assets/js/contact-animations.js
+++ assets/js/contact-animations.js
@@ -2,7 +2,7 @@ var lines = document.querySelectorAll('.dong');
 var contact = document.querySelectorAll('.contact')[0];
 var contactButton = document.querySelectorAll('.social__item--contact')[0];
 var gradientBackground = document.querySelectorAll('.contact__background')[0];
-
+var isOpen = false;
 // contactButton.addEventListener('click', function () {
 //   contact.classList.add('is-open');
 //   // lines.forEach(function (line) {
@@ -12,9 +12,16 @@ var gradientBackground = document.querySelectorAll('.contact__background')[0];
 
 
 module.exports.show = function () {
+  isOpen = true;
   document.body.classList.add('contact-open');
 };
 
 module.exports.hide = function () {
-  document.body.classList.remove('contact-open');
+  if (isOpen) {
+    document.body.classList.remove('contact-open');
+    document.body.classList.add('contact-closed');
+    setTimeout(function () {
+      document.body.classList.remove('contact-closed');
+    }, 1500);  
+  }
 };
